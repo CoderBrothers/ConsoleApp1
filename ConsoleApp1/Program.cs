d923fe9272e5df68237c584c3eeb0f976515f740
CoderBrothers <121336523+CoderBrothers@users.noreply.github.com>
2023-04-04 17:04:01 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,4 @@
+﻿//2 
+//Вывести на экран один куплет любимого стихотворения или песни, с указанием автора или исполнителя. 
+//Используйте литералы (escape-последовательности) для форматирования.
+Console.WriteLine("Cigarettes\nAnd tiny liquor bottles\nJust what you expect\nInside her new Balenciagas\nCigarettes");
